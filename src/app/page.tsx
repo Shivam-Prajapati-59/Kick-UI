b8b95efbd919386b2c95d1d833c8db3fae5d075e
Shivam Prajapati <sp488830@gmail.com>
2026-01-08 15:10:15 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import ShinyButton from "@/components/shimmerButton";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hello Shivam
+      </h1>
+      <ShinyButton />
+    </div>
+
+  );
+}
